ac094b8f38339945738b7a711e28180fa518df85
shakiburcmt <shakibur.cmt@gmail.com>
2022-10-20 19:55:24 +0600

    news

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories=require('./data/categories.json')
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('News API Running');
@@ -13,6 +14,23 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/category/:id', (req, res) => {
+    const categoryId = req.params.id;
+    if (categoryId === '08') {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === categoryId);
+        res.send(categoryNews);
+    }
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
 })
 
 app.listen(port, () => {
